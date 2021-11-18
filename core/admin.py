7f7196e4314547ff9743a6f669aac969c3b506f0
machadodan <danielg_m@hotmail.com>
2021-11-17 21:37:06 -0300

    adcionado funcões de alteração e exclusão de dados

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from core.models import Evento
 # Register your models here.
+
+#defino quais campos quero que aparece no painel admin
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'data_evento', 'data_criacao')
+    list_filter = ('titulo', 'data_evento')
+
+admin.site.register(Evento, EventoAdmin)
+
+
